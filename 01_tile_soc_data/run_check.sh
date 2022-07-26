bc2bfca0ae21722f6db57b8d4a8ba5ca40b3d36c
Pete Bunting <petebunting@mac.com>
2022-07-26 11:17:01 +0100

    Update to use KEA format.

--- 01_tile_soc_data/run_check.sh
+++ 01_tile_soc_data/run_check.sh
@@ -1,4 +1,4 @@
-rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_0cm_30cm/*.tif" --nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_0cm_30cm/*.kea" --nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
 
-rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_30cm_100cm/*.tif" --nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_30cm_100cm/*.kea" --nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
 
