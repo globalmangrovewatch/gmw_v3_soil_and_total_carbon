f24ab4f6d944ee5fb9f8d01a93c022bd367a223e
Pete Bunting <petebunting@mac.com>
2022-07-26 19:32:32 +0100

    update script to check output files are valid.

--- 06_create_total_carbon/run_check.sh
+++ 06_create_total_carbon/run_check.sh
@@ -1,1 +1,5 @@
-python gen_cmds.py --check
+#python gen_cmds.py --check
+
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_soil_total_carbon/data/total_carbon_tiles/2018_2020_total_c/*.tif" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_soil_total_carbon/data/total_carbon_tiles/2018_2020_total_co2/*.tif" --printerrs --rmerr --nbands 1 --epsg 4326 --chkproj --readimg --chksum
+
