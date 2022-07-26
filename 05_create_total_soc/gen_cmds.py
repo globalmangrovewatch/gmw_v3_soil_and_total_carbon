f3fc6062c752f75f01dbfaa28b69c71dfb9feaed
Pete Bunting <petebunting@mac.com>
2022-07-26 17:29:47 +0100

    Add scripts for combining soil carbon levels.

---
+++ 05_create_total_soc/gen_cmds.py
@@ -0,0 +1,68 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+
+import logging
+import os
+import glob
+import tqdm
+import rsgislib.tools.filetools
+import rsgislib.tools.utils
+
+logger = logging.getLogger(__name__)
+
+
+class GenTaskCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs["out_path"]):
+            os.mkdir(kwargs["out_path"])
+
+        imgs = glob.glob(kwargs["img_low_tiles"])
+        for img in tqdm.tqdm(imgs):
+            basename = rsgislib.tools.filetools.get_file_basename(img)
+            tile_name = basename.split("_")[0]
+
+            upper_img = os.path.join(kwargs["img_up_dir"], f"{tile_name}_soil_30_100cm_2018_2020_fill.tif")
+            out_img = os.path.join(kwargs["out_path"], f"{tile_name}_total_soil_2018_2020.tif")
+
+            if not os.path.exists(upper_img):
+                raise Exception("Upper tile not available: {}".format(upper_img))
+
+            if not os.path.exists(out_img):
+                c_dict = dict()
+                c_dict["low_soc_img"] = img
+                c_dict["up_soc_img"] = upper_img
+                c_dict["out_img"] = out_img
+                self.params.append(c_dict)
+
+    def run_gen_commands(self):
+
+        self.gen_command_info(
+            img_low_tiles="/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_0cm_30cm/*.tif",
+            img_up_dir="/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_30cm_100cm/",
+            out_path="/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_total_soc",
+        )
+
+        self.pop_params_db()
+        self.create_shell_exe(
+            run_script="run_exe_analysis.sh",
+            cmds_sh_file="cmds_lst.sh",
+            n_cores=50,
+            db_info_file="pbpt_db_conn_info.json",
+        )
+
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("perform_analysis.py")
+    script_cmd = "python {}".format(py_script)
+
+    process_tools_mod = "perform_analysis"
+    process_tools_cls = "PerformAnalysis"
+
+    create_tools = GenTaskCmds(
+        cmd=script_cmd,
+        db_conn_file="/home/pete/.pbpt_db_conn.txt",
+        lock_file_path="./gmw_lock_file.txt",
+        process_tools_mod=process_tools_mod,
+        process_tools_cls=process_tools_cls,
+    )
+    create_tools.parse_cmds()
---
+++ 05_create_total_soc/perform_analysis.py
@@ -0,0 +1,44 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import rsgislib
+import rsgislib.imageutils
+import rsgislib.imagecalc
+
+logger = logging.getLogger(__name__)
+
+class PerformAnalysis(PBPTQProcessTool):
+    def __init__(self):
+        super().__init__(cmd_name="perform_analysis.py", descript=None)
+
+    def do_processing(self, **kwargs):
+        rsgislib.imageutils.set_env_vars_lzw_gtiff_outs()
+        band_defns = list()
+        band_defns.append(rsgislib.imagecalc.BandDefn('low', self.params["low_soc_img"], 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('up', self.params["up_soc_img"], 1))
+
+        rsgislib.imagecalc.band_math(self.params["out_img"], "low+up", 'GTIFF', rsgislib.TYPE_16UINT, band_defns)
+        rsgislib.imageutils.pop_img_stats(self.params["out_img"], use_no_data=True, no_data_val=0, calc_pyramids=True)
+
+    def required_fields(self, **kwargs):
+        return ["low_soc_img", "up_soc_img", "out_img"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params["out_img"]] = {
+            "type": "gdal_image",
+            "n_bands": 1,
+            "chk_proj": True,
+            "epsg_code": 4326,
+            "read_img": True,
+            "calc_chk_sum": True,
+        }
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        if os.path.exists(self.params["out_img"]):
+            os.remove(self.params["out_img"])
+
+
+if __name__ == "__main__":
+    PerformAnalysis().std_run()
