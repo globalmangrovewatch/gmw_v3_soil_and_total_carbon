a5785a9fd8011ed01361bdef52fc3dc470a3fc94
Pete Bunting <petebunting@mac.com>
2022-07-26 12:11:19 +0100

    Add scripts for calculating the tile stats.

---
+++ 02_calc_tile_stats/gen_cmds.py
@@ -0,0 +1,72 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+
+import logging
+import os
+import glob
+
+import rsgislib.tools.filetools
+
+logger = logging.getLogger(__name__)
+
+
+class GenTaskCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs["out_path"]):
+            os.mkdir(kwargs["out_path"])
+
+        imgs = glob.glob(kwargs["img_tiles"])
+        for img in imgs:
+            basename = rsgislib.tools.filetools.get_file_basename(img)
+            tile_name = basename.split("_")[0]
+            out_file = os.path.join(kwargs["out_path"], f"{basename}_stats.json")
+
+            gmw_tile = os.path.join(kwargs["gmw_ext"], f"{tile_name}_gmw_union.kea")
+            if not os.path.exists(gmw_tile):
+                raise Exception("GMW Tile now available: {}".format(gmw_tile))
+
+            if not os.path.exists(out_file):
+                c_dict = dict()
+                c_dict["tile_img"] = img
+                c_dict["gmw_tile"] = gmw_tile
+                c_dict["out_file"] = out_file
+                self.params.append(c_dict)
+
+    def run_gen_commands(self):
+
+        self.gen_command_info(
+            img_tiles="/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_30cm_100cm/*.tif",
+            gmw_ext="/home/pete/Documents/gmw_v3_soil_total_carbon/data/gmw_v3_extent/gmw_union_srtm_rasters",
+            out_path="/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_30cm_100cm_stats",
+        )
+
+        self.gen_command_info(
+            img_tiles="/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_0cm_30cm/*.tif",
+            gmw_ext="/home/pete/Documents/gmw_v3_soil_total_carbon/data/gmw_v3_extent/gmw_union_srtm_rasters",
+            out_path="/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_0cm_30cm_stats",
+        )
+
+        self.pop_params_db()
+        self.create_shell_exe(
+            run_script="run_exe_analysis.sh",
+            cmds_sh_file="cmds_lst.sh",
+            n_cores=50,
+            db_info_file="pbpt_db_conn_info.json",
+        )
+
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("perform_analysis.py")
+    script_cmd = "python {}".format(py_script)
+
+    process_tools_mod = "perform_analysis"
+    process_tools_cls = "PerformAnalysis"
+
+    create_tools = GenTaskCmds(
+        cmd=script_cmd,
+        db_conn_file="/home/pete/.pbpt_db_conn.txt",
+        lock_file_path="./gmw_lock_file.txt",
+        process_tools_mod=process_tools_mod,
+        process_tools_cls=process_tools_cls,
+    )
+    create_tools.parse_cmds()
---
+++ 02_calc_tile_stats/perform_analysis.py
@@ -0,0 +1,64 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import rsgislib
+import rsgislib.tools.utils
+
+import numpy
+import osgeo.gdal as gdal
+
+logger = logging.getLogger(__name__)
+
+def calc_stats(vals_img, gmw_img):
+    img_vals_ds = gdal.Open(vals_img)
+    if img_vals_ds is None:
+        raise Exception("Could not open the values input image: '{}'".format(uid_img))
+    img_vals_band = img_vals_ds.GetRasterBand(1)
+    if img_vals_band is None:
+        raise Exception("Failed to read the values image band: '{}'".format(uid_img))
+    vals_arr = img_vals_band.ReadAsArray()
+    img_vals_ds = None
+
+    img_gmw_ds = gdal.Open(gmw_img)
+    if img_gmw_ds is None:
+        raise Exception("Could not open the GMW input image: '{}'".format(gmw_img))
+    img_gmw_band = img_gmw_ds.GetRasterBand(1)
+    if img_gmw_band is None:
+        raise Exception("Failed to read the GMW image band: '{}'".format(gmw_img))
+    gmw_arr = img_gmw_band.ReadAsArray()
+    img_gmw_ds = None
+
+    msk = numpy.zeros_like(gmw_arr, dtype=bool)
+    msk[(vals_arr > 0) & (gmw_arr == 1)] = True
+
+    val_stats_lut = dict()
+    val_stats_lut['count'] = numpy.sum(msk)
+    val_stats_lut['sum'] = numpy.sum(vals_arr[msk])
+
+    return val_stats_lut
+
+
+class PerformAnalysis(PBPTQProcessTool):
+    def __init__(self):
+        super().__init__(cmd_name="perform_analysis.py", descript=None)
+
+    def do_processing(self, **kwargs):
+        stats_lut = calc_stats(self.params["tile_img"], self.params["gmw_tile"])
+
+        rsgislib.tools.utils.write_dict_to_json(stats_lut, self.params["out_file"])
+
+    def required_fields(self, **kwargs):
+        return ["tile_img", "gmw_tile", "out_file"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params["out_file"]] = "file"
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        if os.path.exists(self.params["out_file"]):
+            os.remove(self.params["out_file"])
+
+
+if __name__ == "__main__":
+    PerformAnalysis().std_run()
