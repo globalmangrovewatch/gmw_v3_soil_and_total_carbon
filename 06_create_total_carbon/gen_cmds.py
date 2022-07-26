2e806aa1bc08a839e3702a3c17b3e45a7794c096
Pete Bunting <petebunting@mac.com>
2022-07-26 17:49:11 +0100

    Add scripts to calculate the total carbon and CO2 equivlent.

---
+++ 06_create_total_carbon/gen_cmds.py
@@ -0,0 +1,80 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+
+import logging
+import os
+import glob
+import tqdm
+import rsgislib.tools.filetools
+import rsgislib.tools.utils
+
+logger = logging.getLogger(__name__)
+
+
+class GenTaskCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs["out_c_path"]):
+            os.mkdir(kwargs["out_c_path"])
+
+        if not os.path.exists(kwargs["out_co2_path"]):
+            os.mkdir(kwargs["out_co2_path"])
+
+        imgs = glob.glob(kwargs["img_tiles"])
+        for img in tqdm.tqdm(imgs):
+            basename = rsgislib.tools.filetools.get_file_basename(img)
+            tile_name = basename.split("_")[0]
+
+            agb_img = os.path.join(kwargs["agb_dir"], f"{tile_name}_agb_gmw_v314_mng_mjr_2020.tif")
+            gmw_img = os.path.join(kwargs["gmw_ext_dir"], f"{tile_name}_gmw_v314_mng_mjr_2020.kea")
+            out_c_img = os.path.join(kwargs["out_c_path"], f"{tile_name}_total_c_2018_2020.tif")
+            out_co2_img = os.path.join(kwargs["out_co2_path"], f"{tile_name}_total_co2_2018_2020.tif")
+
+            if not os.path.exists(agb_img):
+                raise Exception("AGB tile not available: {}".format(agb_img))
+
+            if not os.path.exists(gmw_img):
+                raise Exception("gmw tile not available: {}".format(gmw_img))
+
+            if not os.path.exists(out_c_img) or not os.path.exists(out_co2_img):
+                c_dict = dict()
+                c_dict["soc_img"] = img
+                c_dict["agb_img"] = agb_img
+                c_dict["gmw_img"] = gmw_img
+                c_dict["out_c_img"] = out_c_img
+                c_dict["out_co2_img"] = out_co2_img
+                self.params.append(c_dict)
+
+    def run_gen_commands(self):
+
+        self.gen_command_info(
+            img_tiles="/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_total_soc/*.tif",
+            agb_dir="/home/pete/Documents/gmw_v3_soil_total_carbon/data/agb_mng_mjr_2020_tif",
+            gmw_ext_dir="/home/pete/Documents/gmw_v3_soil_total_carbon/data/gmw_v3_extent/mng_mjr_2020",
+            out_c_path="/home/pete/Documents/gmw_v3_soil_total_carbon/data/total_carbon_tiles/2018_2020_total_c",
+            out_co2_path="/home/pete/Documents/gmw_v3_soil_total_carbon/data/total_carbon_tiles/2018_2020_total_co2",
+        )
+
+        self.pop_params_db()
+        self.create_shell_exe(
+            run_script="run_exe_analysis.sh",
+            cmds_sh_file="cmds_lst.sh",
+            n_cores=50,
+            db_info_file="pbpt_db_conn_info.json",
+        )
+
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("perform_analysis.py")
+    script_cmd = "python {}".format(py_script)
+
+    process_tools_mod = "perform_analysis"
+    process_tools_cls = "PerformAnalysis"
+
+    create_tools = GenTaskCmds(
+        cmd=script_cmd,
+        db_conn_file="/home/pete/.pbpt_db_conn.txt",
+        lock_file_path="./gmw_lock_file.txt",
+        process_tools_mod=process_tools_mod,
+        process_tools_cls=process_tools_cls,
+    )
+    create_tools.parse_cmds()
---
+++ 06_create_total_carbon/perform_analysis.py
@@ -0,0 +1,58 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import rsgislib
+import rsgislib.imageutils
+import rsgislib.imagecalc
+
+logger = logging.getLogger(__name__)
+
+class PerformAnalysis(PBPTQProcessTool):
+    def __init__(self):
+        super().__init__(cmd_name="perform_analysis.py", descript=None)
+
+    def do_processing(self, **kwargs):
+        rsgislib.imageutils.set_env_vars_lzw_gtiff_outs()
+        band_defns = list()
+        band_defns.append(rsgislib.imagecalc.BandDefn('soc', self.params["soc_img"], 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('agb', self.params["agb_img"], 1))
+
+        rsgislib.imagecalc.band_math(self.params["out_c_img"], "soc+(agb*0.451)", 'GTIFF', rsgislib.TYPE_16UINT, band_defns)
+        rsgislib.imageutils.pop_img_stats(self.params["out_c_img"], use_no_data=True, no_data_val=0, calc_pyramids=True)
+
+        rsgislib.imagecalc.image_math(self.params["out_c_img"], self.params["out_co2_img"], "b1*3.67", 'GTIFF', rsgislib.TYPE_16UINT)
+        rsgislib.imageutils.pop_img_stats(self.params["out_co2_img"], use_no_data=True, no_data_val=0, calc_pyramids=True)
+
+    def required_fields(self, **kwargs):
+        return ["soc_img", "agb_img", "gmw_img", "out_c_img", "out_co2_img"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params["out_c_img"]] = {
+            "type": "gdal_image",
+            "n_bands": 1,
+            "chk_proj": True,
+            "epsg_code": 4326,
+            "read_img": True,
+            "calc_chk_sum": True,
+        }
+        files_dict[self.params["out_c_img"]] = {
+            "type": "gdal_image",
+            "n_bands": 1,
+            "chk_proj": True,
+            "epsg_code": 4326,
+            "read_img": True,
+            "calc_chk_sum": True,
+        }
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        if os.path.exists(self.params["out_c_img"]):
+            os.remove(self.params["out_c_img"])
+
+        if os.path.exists(self.params["out_co2_img"]):
+            os.remove(self.params["out_co2_img"])
+
+
+if __name__ == "__main__":
+    PerformAnalysis().std_run()
