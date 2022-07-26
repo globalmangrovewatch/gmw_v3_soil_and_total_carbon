683db34878d97b0ad01449fd4a0ec72e8e92b191
Pete Bunting <petebunting@mac.com>
2022-07-26 11:14:36 +0100

    Update to check images are correct.

--- 01_tile_soc_data/run_check.sh
+++ 01_tile_soc_data/run_check.sh
@@ -1,4 +1,4 @@
-rsgischkgdalfile.py -i "/home/pete/Documents/gmw_protected_areas/data/gmw_ext_protect_areas/*/extent/*.kea" --nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_0cm_30cm/*.tif" --nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
 
-rsgischkgdalfile.py -i "/home/pete/Documents/gmw_protected_areas/data/gmw_srtm_protect_areas/*/extent/*.kea" --nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
+rsgischkgdalfile.py -i "/home/pete/Documents/gmw_v3_soil_total_carbon/data/soil_carbon_tiles/2018_2020_30cm_100cm/*.tif" --nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
 
