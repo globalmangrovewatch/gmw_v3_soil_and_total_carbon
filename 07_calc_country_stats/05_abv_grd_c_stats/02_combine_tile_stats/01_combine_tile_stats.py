c8aa61ac14c94bb5fbbec31304f0cd42433ac980
Pete Bunting <petebunting@mac.com>
2023-02-08 21:31:48 +0000

    Update to paths.

--- 07_calc_country_stats/05_abv_grd_c_stats/02_combine_tile_stats/01_combine_tile_stats.py
+++ 07_calc_country_stats/05_abv_grd_c_stats/02_combine_tile_stats/01_combine_tile_stats.py
@@ -23,7 +23,7 @@ for val in unq_cntry_vals:
     tile_stats_lut[val]['vals_area'] = 0.0
     tile_hist_lut[val] = numpy.zeros((201), dtype=numpy.uint32)
 
-stats_tiles = glob.glob('/home/pete/Documents/gmw_v3_soil_total_carbon/data/gmw_v2_soil_carbon_data/out_stats/gmw_2020_ag_c/tile_stats/*.json')
+stats_tiles = glob.glob('/home/pete/Documents/gmw_v3_soil_total_carbon/data/soc_20221216/out_stats/gmw_2020_ag_c/tile_stats/*.json')
 
 for stats_tile_file in stats_tiles:
     stats_tile_lut = rsgislib.tools.utils.read_json_to_dict(stats_tile_file)
